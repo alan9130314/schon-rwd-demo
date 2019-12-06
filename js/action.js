055e7052bd2a086c5eca75f4ab7c5a615cc77a44
Alan Wang <alan9130314@gmail.com>
2019-12-06 18:01:45 +0800

    add product and shoppingcart pages

--- js/action.js
+++ js/action.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+
+  var modal = document.getElementById("modal-container");
+
+  
+  
+  $('.basket').click(function(e){
+    modal.style.display = "block";
+    
+  });
+
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
   $('.owl-carousel').owlCarousel({
     stagePadding: 50,
     center: true,
@@ -32,8 +50,7 @@ $(document).ready(function () {
     slidesToScroll: 1,
     prevArrow: $('.sl-prev'),
     nextArrow: $('.sl-next'),
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -43,7 +60,7 @@ $(document).ready(function () {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow:3,
+          slidesToShow: 3,
           slidesToScroll: 1
         }
       },
@@ -58,7 +75,7 @@ $(document).ready(function () {
       // settings: "unslick"
       // instead of a settings object
     ]
-  
+
 
   });
 
@@ -70,8 +87,7 @@ $(document).ready(function () {
     slidesToScroll: 1,
     prevArrow: $('.sli-prev'),
     nextArrow: $('.sli-next'),
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -96,9 +112,62 @@ $(document).ready(function () {
       // settings: "unslick"
       // instead of a settings object
     ]
-  
+  });
+
+  $('.carousel-related').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: $('.sl-prev'),
+    nextArrow: $('.sl-next'),
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+
 
   });
+
+
+  $("#example_id").ionRangeSlider();
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 10,
+    to: 599,
+    skin: "round",
+    onChange: function (data) {
+      $('span.PFfrom').text(('$') + data.from);
+      $('span.PFto').text(('$') + data.to);
+    }
+  });
+
+  
+  
 
 
 
